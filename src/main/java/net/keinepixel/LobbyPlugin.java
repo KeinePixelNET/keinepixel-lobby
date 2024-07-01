8b11644ae3a82465dc828f33d76bbf537a1fc83a
KeineSecrets <miethe.max@web.de>
2024-07-01 19:33:51 +0200

    Changed api version to 0.0.2

--- src/main/java/net/keinepixel/LobbyPlugin.java
+++ src/main/java/net/keinepixel/LobbyPlugin.java
@@ -4,8 +4,18 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LobbyPlugin extends JavaPlugin {
 
+    PixelAPI pixelAPI;
+
     @Override
     public void onEnable() {
+
+        pixelAPI = (PixelAPI) getServer().getPluginManager().getPlugin("PixelAPI");
+        if (pixelAPI == null) {
+            getLogger().severe("PixelAPI not found! Disabling plugin...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
 
     }
 
